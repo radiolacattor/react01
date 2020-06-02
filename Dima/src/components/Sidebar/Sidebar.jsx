26379f8257835d9f87bc6aee0ebe8f905378e4af
root <root@localhost.localdomain>
2020-06-02 10:33:02 +0300

    sidebar

--- Dima/src/components/Sidebar/Sidebar.jsx
+++ Dima/src/components/Sidebar/Sidebar.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import s from './Sidebar.module.css'
-<<<<<<< HEAD
 import SidebarEl from "./SidebarEl/SidebarEl";
 
 const Sidebar = (props) => {
@@ -10,22 +9,6 @@ const Sidebar = (props) => {
         <div className={s.friendWrapper}>
             <div className={s.statusFriend}>Online</div>
             {usersOnline}
-=======
-
-const Sidebar = (props) => {
-    return (
-        <div className={s.friendWrapper}>
-            <div className={s.statusFriend}>Online</div>
-            <div className={s.friend}>
-                <p>Dima</p>
-            </div>
-            <div className={s.friend}>
-                <p>Ajnura</p>
-            </div>
-            <div className={s.friend}>
-                <p>Kirill</p>
-            </div>
->>>>>>> 04d36e0a8e06ab2bbd258aacbe8ca957c02a267d
         </div>
     )
 }
