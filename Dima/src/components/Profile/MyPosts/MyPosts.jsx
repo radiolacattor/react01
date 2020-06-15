71f85f961fb779909469f0f56bce079708b7dc9e
unknown <radiolacattor@gmail.com>
2020-06-15 23:26:52 +0300

    FLUX

--- Dima/src/components/Profile/MyPosts/MyPosts.jsx
+++ Dima/src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,21 +3,25 @@ import s from './MyPosts.module.css';
 import MyPost from './Post/MyPost';
 
 const MyPosts = (props) => {
+
     let postsElements = props.posts.map(p => <MyPost like={p.like} message={p.message}/>);
 
     let newPostElement = React.createRef();
 
     let addPost = () => {
+        props.addPost();
+    }
+
+    let onPostChange = () => {
         let text = newPostElement.current.value;
-        props.addPost(text);
-        newPostElement.current.value = '';
+        props.updateNewPostText(text);
     }
 
     return (
         <div className={s.postsBlock}>
             <h3>My posts</h3>
             <div>
-                <textarea ref={newPostElement}></textarea>
+                <textarea onChange={onPostChange} ref={newPostElement} value={props.newPostText}/>
             </div>
             <div>
                 <button onClick={addPost}>Add Post</button>
--- Dima/src/redux/state.js
+++ Dima/src/redux/state.js
@@ -6,7 +6,8 @@ let state = {
         posts: [
             {id: 1, like: 15, message: 'Hi, fuck you!!!'},
             {id: 2, like: 30, message: 'Hi, fuck you too!!! Motherfucker!!!'}
-        ]
+        ],
+        newPostText: ''
     },
 
     dialogsPage: {
@@ -21,7 +22,8 @@ let state = {
             {id: 1, msg: 'Miay'},
             {id: 2, msg: 'How are your miay?'},
             {id: 3, msg: 'Thx, very myr!'}
-        ]
+        ],
+        newMessageText: ''
     },
 
     sidebar: {
@@ -34,14 +36,36 @@ let state = {
 
 }
 
-export let addPost = (postMessage) => {
+export let addPost = () => {
     let newPost = {
         id: 3,
         like: 0,
-        message: postMessage
+        message: state.profilePage.newPostText
     };
     state.profilePage.posts.push(newPost);
+    state.profilePage.newPostText = '';
     rerenderEntireTree(state);
 }
+
+export let updateNewPostText = (newText) => {
+    state.profilePage.newPostText = newText;
+    rerenderEntireTree(state);
+}
+
+export let addMessage = () => {
+    let newMessage = {
+        id: 4,
+        msg: state.dialogsPage.newMessageText
+    };
+    state.dialogsPage.messages.push(newMessage);
+    state.dialogsPage.newMessageText = '';
+    rerenderEntireTree(state);
+}
+
+export let updateNewMessageText = (newText) => {
+    state.dialogsPage.newMessageText = newText;
+    rerenderEntireTree(state);
+}
+
 
 export default state;
