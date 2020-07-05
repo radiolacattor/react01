1c11a4fe28232cec3d50bb66e30f1faffdca3856
Norm <radiolacattor@gmail.com>
2020-07-05 20:04:53 +0300

    ajax

--- Dima/src/components/Users/Users.jsx
+++ Dima/src/components/Users/Users.jsx
@@ -1,46 +1,15 @@
 import React from "react";
-import s from './Users.module.css'
+import s from './Users.module.css';
+import * as axios from 'axios';
 
 const Users = (props) => {
     if (props.users.length === 0) {
-        props.setUsers([
-                {
-                    id: 1,
-                    avatarURL: 'https://cdn.ren.tv/cache/960x540/media/img/14/46/144659e6d12aa348c7eae2170d1d6e04f3d2d1da.jpg',
-                    followed: true,
-                    fullName: 'Dima',
-                    status: 'Big boss',
-                    location:
-                        {city: 'Minsk', country: 'Belarus'}
-                },
-                {
-                    id: 2,
-                    avatarURL: 'https://cdn.ren.tv/cache/960x540/media/img/14/46/144659e6d12aa348c7eae2170d1d6e04f3d2d1da.jpg',
-                    followed: false,
-                    fullName: 'Ajnura',
-                    status: 'Little miayka',
-                    location:
-                        {city: 'Minsk', country: 'Belarus'}
-                },
-                {
-                    id: 3,
-                    avatarURL: 'https://cdn.ren.tv/cache/960x540/media/img/14/46/144659e6d12aa348c7eae2170d1d6e04f3d2d1da.jpg',
-                    followed: true,
-                    fullName: 'Kirill',
-                    status: 'Little boss',
-                    location:
-                        {city: 'Minsk', country: 'Belarus'}
-                },
-                {
-                    id: 4,
-                    avatarURL: 'https://cdn.ren.tv/cache/960x540/media/img/14/46/144659e6d12aa348c7eae2170d1d6e04f3d2d1da.jpg',
-                    followed: false,
-                    fullName: 'Barsik',
-                    status: 'Awful cat',
-                    location:
-                        {city: 'Minsk', country: 'Belarus'}
-                }
-            ])
+
+        axios
+            .get("https://agrokredit.keenetic.pro/users.php").then(response => {
+            debugger
+            props.setUsers()
+        })
     }
 
     return (
